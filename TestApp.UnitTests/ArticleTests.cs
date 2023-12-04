e6d0bf82117ad362cb6dc94e6b00e4b0e97c3e48
emildinkov <135504796+emildinkov@users.noreply.github.com>
2023-12-04 13:36:22 +0200

    Add project files.

---
+++ TestApp.UnitTests/ArticleTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class ArticleTests
+{
+    private Article _article;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._article = new Article();
+    }
+
+    [Test]
+    public void Test_AddArticles_ReturnsArticleWithCorrectData()
+    {
+        // Arrange
+        string[] articleData =
+        {
+            "Article Content Author",
+            "Article Content2 Author",
+            "Artical Content Author3",
+        };
+
+        // Act
+        Article result = this._article.AddArticles(articleData);
+
+        // Assert
+        Assert.That(result.ArticleList, Has.Count.EqualTo(3));
+        Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article"));
+        Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
+        Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
+    }
+
+    [Test]
+    public void Test_GetArticleList_SortsArticlesByTitle()
+    {
+        // Arrange
+        Article input = new Article()
+        {
+            ArticleList = new()
+            {
+                new Article()
+                {
+                    Author = "Emil",
+                    Content = "Some content",
+                    Title = "Title"
+                },
+                new Article()
+                {
+                    Author = "Emil's",
+                    Content = "Some content",
+                    Title = "Above"
+                }
+            }
+        };
+
+        string printCriteria = "title";
+
+        string expected = $"Above - Some content: Emil's{Environment.NewLine}Title - Some content: Emil";
+
+        // Act
+        string actual = this._article.GetArticleList(input, printCriteria);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
+    {
+        // Arrange
+        Article input = new Article()
+        {
+            ArticleList = new()
+            {
+                new Article()
+                {
+                    Author = "Emil",
+                    Content = "Some content",
+                    Title = "Title"
+                },
+                new Article()
+                {
+                    Author = "Emil's",
+                    Content = "Some content",
+                    Title = "Above"
+                }
+            }
+        };
+
+        string printCriteria = "futbol";
+
+        // Act
+        string actual = this._article.GetArticleList(input, printCriteria);
+
+        // Assert
+        Assert.That(actual, Is.Empty);
+    }
+}
---
+++ TestApp.UnitTests/SongTests.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class SongTests
+{
+    private Song _song;
+
+    [SetUp]
+    public void Setup()
+    {
+        this._song = new();
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsAllSongs_WhenWantedListIsAll()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string expected = $"Song1{Environment.NewLine}Song2{Environment.NewLine}Song3";
+        string wantedList = "all";
+
+        // Act
+        string result = this._song.AddAndListSongs(songs, wantedList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsFilteredSongs_WhenWantedListIsSpecific()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string expected = "Song2";
+        string wantedList = "Rock";
+
+        // Act
+        string result = this._song.AddAndListSongs(songs, wantedList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsEmptyString_WhenNoSongsMatchWantedList()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string expected = string.Empty;
+        string wantedList = "Hip-hop";
+
+        // Act
+        string result = this._song.AddAndListSongs(songs, wantedList);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
